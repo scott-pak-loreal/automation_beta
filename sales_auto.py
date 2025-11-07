b66c0352b6d5f07ad42c7e678a93c865a22702a9
fernando-gonzalez-loreal <fernando.luisgonzalezmaldonado@loreal.com>
2025-11-07 13:38:35 -0600

    Added Biolage Sales Data

--- sales_auto.py
+++ sales_auto.py
@@ -1,6 +1,6 @@
 
 import pandas as pd
 # Load the Excel file
-df = pd.read_excel("Test 1.xlsx")
+df = pd.read_excel("Biolage Sales Data.xlsx", sheet_name="Raw Data")
 # Display first 5 rows
 print(df.head())
